a1ebc52df2484597dfbe0d745b765cf7f6a6ab1c
gareth leong <gareth.leong@fdmgroup.com>
2021-05-13 11:30:13 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import List from './components/List';
+import Add from './components/Add';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+	constructor(){
+
+		super();
+
+		this.state = {
+
+			todos:[],
+			index:0
+		};
+	}
+
+	render(){
+
+		return(
+			<div>
+				<Add addFunction={this.add}></Add>
+				<List completeFunction={this.complete} deleteFunction={this.delete} editFunction={this.edit} todos={this.state.todos}></List>
+			</div>
+		)
+	}
+
+	componentDidMount = () => {
+
+		const todos = localStorage.getItem('todos');
+		const index = localStorage.getItem('index');
+		
+		if(todos) {
+
+			const todo = JSON.parse(todos);
+			const indexes = JSON.parse(index);
+			this.setState({todos : todo});
+			this.setState({index : indexes})
+		}
+	}
+
+	add = async (todo) => {
+
+		this.setState({index: this.state.index + 1});
+		await this.setState({
+			todos:[...this.state.todos, {
+				index: this.state.index,
+				text: todo, 
+				completed: false
+			}]
+		})
+
+		localStorage.setItem('todos', JSON.stringify(this.state.todos));
+		localStorage.setItem('index', JSON.stringify(this.state.index));
+	}
+
+	complete = async (todo) => {
+
+		const newToDos = this.state.todos.map(originalToDo => {
+		
+			if (todo === originalToDo)
+				return {
+
+					index: todo.index,
+					text: todo.text,
+					completed: !todo.completed
+				}
+
+			else return originalToDo
+		});
+
+		await this.setState({todos: newToDos})
+		console.log(newToDos)
+
+		localStorage.setItem('todos', JSON.stringify(this.state.todos));
+	}
+
+	delete = async (todo) => {
+
+		this.setState({todos:[...this.state.todos]});
+		var newToDos = [...this.state.todos];
+
+		var index = newToDos.indexOf(todo);
+
+		if (index !== -1){
+
+			newToDos.splice(index, 1);
+			await this.setState({todos: newToDos});
+		}
+
+		localStorage.setItem('todos', JSON.stringify(this.state.todos));
+	}
+	
+	edit = async (todo, edit) => {
+
+		const newToDos = this.state.todos.map(originalToDo => {
+		
+			if (todo === originalToDo)
+				return {
+
+					index: todo.index,
+					text: edit,
+					completed: todo.completed
+				}
+
+			else return originalToDo
+		});
+
+		await this.setState({todos: newToDos})
+		console.log(newToDos)
+
+		localStorage.setItem('todos', JSON.stringify(this.state.todos));
+	}
+
 }
 
 export default App;
